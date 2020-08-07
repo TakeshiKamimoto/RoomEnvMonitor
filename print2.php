ccc505ed54696c9e01fd21b8a06900300581d4a9
TakeshiKamimoto <takeshikamimoto@gmail.com>
2020-08-07 15:26:30 +0900

    added print.php

---
+++ print2.php
@@ -0,0 +1,15 @@
+<?php
+
+//データーベースに接続
+$con = new PDO("sqlite:/home/pi/LogMonitor/measure.db");
+
+//
+$sql = "SELECT timestamp,temp,temp_m,humid,press,discomfort FROM weather WHERE timestamp>datetime('now','-16 hour')";
+$res = $con->query($sql);
+
+while( $val = $res->fetch(PDO::FETCH_ASSOC) ){
+  $json[] = $val;
+}
+
+//jsonで出力
+echo json_encode( $json );
